770d71041c1113a852200df4eb20f6634ff73ed1
Matt Huston <matthuston1@aol.com>
2018-08-14 15:13:24 -0600

    made queries more dynamic, changed routes to match

--- routes/supplies.js
+++ routes/supplies.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const queries = require('../queries');
 
 router.get("/", (request, response, next) => {
-    queries.listSupplies().then(supplies => {
+    queries.list("supplies").then(supplies => {
         response.json({supplies});
     }).catch(next);
 });
 
 router.get("/:id", (request, response, next) => {
-    queries.readSupplies(request.params.id).then(supplies => {
+    queries.read("supplies", request.params.id).then(supplies => {
         supplies
             ? response.json({supplies})
             : response.status(404).json({message: 'Not found'})
@@ -18,19 +18,19 @@ router.get("/:id", (request, response, next) => {
 });
 
 router.post("/", (request, response, next) => {
-    queries.createSupplies(request.body).then(supplies => {
+    queries.create("supplies", request.body).then(supplies => {
         response.status(201).json({supplies: supplies});
     }).catch(next);
 });
 
 router.delete("/:id", (request, response, next) => {
-    queries.deleteSupplies(request.params.id).then(() => {
+    queries.delete("supplies", request.params.id).then(() => {
         response.status(204).json({deleted: true});
     }).catch(next);
 });
 
 router.put("/:id", (request, response, next) => {
-    queries.updateSupplies(request.params.id, request.body).then(supplies => {
+    queries.update("supplies", request.params.id, request.body).then(supplies => {
         response.json({supplies: supplies[0]});
     }).catch(next);
 });
--- routes/teachers.js
+++ routes/teachers.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const queries = require('../queries');
 
 router.get("/", (request, response, next) => {
-    queries.listTeachers().then(teachers => {
+    queries.list("teachers").then(teachers => {
         response.json({teachers});
     }).catch(next);
 });
 
 router.get("/:id", (request, response, next) => {
-    queries.readTeachers(request.params.id).then(teachers => {
+    queries.read("teachers",request.params.id).then(teachers => {
         teachers
             ? response.json({teachers})
             : response.status(404).json({message: 'Not found'})
@@ -18,19 +18,19 @@ router.get("/:id", (request, response, next) => {
 });
 
 router.post("/", (request, response, next) => {
-    queries.createTeachers(request.body).then(teachers => {
+    queries.create("teachers",request.body).then(teachers => {
         response.status(201).json({teachers: teachers});
     }).catch(next);
 });
 
 router.delete("/:id", (request, response, next) => {
-    queries.deleteTeachers(request.params.id).then(() => {
+    queries.delete("teachers",request.params.id).then(() => {
         response.status(204).json({deleted: true});
     }).catch(next);
 });
 
 router.put("/:id", (request, response, next) => {
-    queries.updateTeachers(request.params.id, request.body).then(teachers => {
+    queries.update("teachers",request.params.id, request.body).then(teachers => {
         response.json({teachers: teachers[0]});
     }).catch(next);
 });
